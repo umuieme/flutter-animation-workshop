1c82832e36e5e3660eb0c053310b3d5dd33cebb4
Umesh Basnet <basnet0umesh@gmail.com>
2019-09-03 00:13:05 +0545

    animate on place detail enter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_workshop_animation/model/place_model.dart';
+import 'package:flutter_workshop_animation/place_detail/place_detail_screen.dart';
  
 void main() => runApp(MyApp());
  
@@ -7,15 +9,32 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
+      home: new HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Material App Bar'),
+      ),
+      body: Center(
+        child:  RaisedButton(
+        child: Text("press"),
+        onPressed: () => Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => PlaceDetailScreen(
+                      placeModel: PlaceModel.getDummyPlace(),
+                    ))),
+      ),
       ),
     );
   }
---
+++ lib/place_detail/place_detail_screen.dart
@@ -0,0 +1,43 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_workshop_animation/model/place_model.dart';
+import 'package:flutter_workshop_animation/place_detail/place_detail.dart';
+import 'package:flutter_workshop_animation/place_detail/place_detail_enter_animation.dart';
+
+class PlaceDetailScreen extends StatefulWidget {
+  final PlaceModel placeModel;
+
+  const PlaceDetailScreen({Key key, @required this.placeModel})
+      : super(key: key);
+
+  @override
+  _PlaceDetailScreenState createState() =>
+      _PlaceDetailScreenState(this.placeModel);
+}
+
+class _PlaceDetailScreenState extends State<PlaceDetailScreen> with SingleTickerProviderStateMixin {
+  PlaceModel placeModel;
+
+  AnimationController _controller;
+
+  _PlaceDetailScreenState(this.placeModel);
+
+  @override
+  void initState() { 
+    super.initState();
+    _controller = AnimationController(duration: Duration(milliseconds: 1000), vsync: this);
+    _controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PlaceDetail(placeModel: placeModel, controller: _controller,);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+}
